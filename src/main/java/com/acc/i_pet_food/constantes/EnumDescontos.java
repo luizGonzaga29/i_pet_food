844df5021cbc661b96390b01cc1e98f340b96a88
luizGonzaga29 <luizgonzagacabral29@gmail.comgit config --global user.email luizgonzagacabral29@gmail.com>
2021-10-04 15:33:32 -0300

    Aficionando constantes

--- src/main/java/com/acc/i_pet_food/constantes/EnumDescontos.java
+++ src/main/java/com/acc/i_pet_food/constantes/EnumDescontos.java
@@ -1,5 +1,18 @@
 package com.acc.i_pet_food.constantes;
 
-public class EnumDescontos {
+public enum EnumDescontos {
 
+	ACIMA300(0.85),
+	ENTE200E300(0.9),
+	ENTRE100E200(0.95);
+
+	private double desconto;
+
+	EnumDescontos(double desconto) {
+		this.desconto = desconto;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
 }
